bd7edffa26ed84578da85eb4914a498f2194d601
madhead <siarhei.krukau@gmail.com>
2021-11-21 20:13:18 +0100

    Init a simple Spring Boot app

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    alias(libs.plugins.boot)
+    alias(libs.plugins.dependency.management)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.testcontainers:testcontainers-bom:${libs.versions.testcontainers.get()}")
+    }
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            showStandardStreams = true
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
+import org.gradle.api.internal.FeaturePreviews.Feature.VERSION_CATALOGS
+
 rootProject.name = "quotes"
+
+enableFeaturePreview(VERSION_CATALOGS.name)
