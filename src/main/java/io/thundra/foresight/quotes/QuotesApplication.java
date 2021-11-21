835c179c9c0134ede5604ee04613923446db6744
madhead <siarhei.krukau@gmail.com>
2021-11-21 20:23:37 +0100

    Simple quotes endpoints stubs

--- src/main/java/io/thundra/foresight/quotes/QuotesApplication.java
+++ src/main/java/io/thundra/foresight/quotes/QuotesApplication.java
@@ -2,8 +2,10 @@ package io.thundra.foresight.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class QuotesApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuotesApplication.class, args);
