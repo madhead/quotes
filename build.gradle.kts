e34a77b4742dbc59aa46d692bbb8b0772e964de8
madhead <siarhei.krukau@gmail.com>
2021-12-15 01:19:07 +0100

    Add junit-platform-runner dependency to the test classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-runner")
 }
 
 java {
